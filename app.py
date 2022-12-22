85c4cfdc41d0bbb9cf8b34b9fe79cf03f12edd7e
Noctwis <60947055+Noctwis@users.noreply.github.com>
2022-12-22 09:15:32 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def main() :
     #Title display
     html_temp = """
     <div style="background-color: tomato; padding:10px; border-radius:10px">
-    <h1 style="color: white; text-align:center">Dashboard Scoring Credit fait par Wissem, Orkun le BG, Daline la bête et Jaures</h1>
+    <h1 style="color: white; text-align:center">Dashboard Scoring Credit fait par Wissem Ben Chelbi</h1>
     </div>
     <p style="font-size: 20px; font-weight: bold; text-align:center">Credit decision support…</p>
     """
