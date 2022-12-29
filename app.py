1078832e6ba1d81c5c63398849c01eb775dcd932
Noctwis <60947055+Noctwis@users.noreply.github.com>
2022-12-29 11:53:35 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -287,7 +287,7 @@ def main() :
         
             shap.initjs()
             shap.force_plot(explainer.expected_value[1], shap_values[1], data_for_prediction)
-            link='logit', matplotlib=True, figsize=(12,3))
+            link='logit', matplotlib=True, figsize=(12,3)
             st.pyplot(bbox_inches='tight',dpi=300,pad_inches=0)
             plt.clf()
         
