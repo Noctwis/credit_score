308c39eab65b909b3a3c89ca35fab69e62e67be7
Noctwis <60947055+Noctwis@users.noreply.github.com>
2025-07-09 16:40:43 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -265,7 +265,8 @@ def main() :
         explainer = shap.TreeExplainer(load_model())
         
         shap_values = explainer.shap_values(X)
-        shap.force_plot(shap_values[0], X, plot_type ="bar", max_display=number, color_bar=False, plot_size=(5, 5))
+        #summary_plot(shap_values[0], X, plot_type ="bar", max_display=number, color_bar=False, plot_size=(5, 5))
+        shap.force_plot(explainer.expected_value, shap_values[0])
         st.pyplot(fig)
         
 
